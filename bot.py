1d40e5ef3a7df160e1419fe393bdc5c758050518
YarinTheDev <110265000+YarinTheDev@users.noreply.github.com>
2022-10-04 12:05:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
+import sys
+import subprocess
+
+
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'webbrowser'])
+  
 import webbrowser
 
+
+
 webbrowser.open('https://pornhub.com')
+
